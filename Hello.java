b63d0d8ac7427ce95290d5abdcf901618d1f1d88
Haydenamacio <89475529+Haydenamacio@users.noreply.github.com>
2021-08-24 12:45:36 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/********************************************************************************************
+* Java Program Name
+*********************************************************************************************
+* Student Name (Hayden Amacio)
+* 08/24/2021
+* CMCS 255
+*********************************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	System.out.println("<Your Name>");
+	System.out.println("<Java Program Name>");
+	System.out.println("<Program/Project Description>");
+	System.out.println("<Date Created>");
+	System.out.println("<Course Number and Section Number>");
+	System.out.println();
+	}
+} 
